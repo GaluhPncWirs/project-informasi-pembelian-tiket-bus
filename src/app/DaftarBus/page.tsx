2808c3db903f6ee4552e14000b4012105b843b8e
galuh panca wirasa <galuhpanca166@gmail.com>
2026-03-24 10:05:50 +0700

    feat: initial commit

--- src/app/DaftarBus/page.tsx
+++ src/app/DaftarBus/page.tsx
@@ -5,17 +5,17 @@ import RootLayout from "../../layout/rootLayout/content";
 import { Checkbox } from "../../components/ui/checkbox";
 
 export default function DaftarBus() {
-  const MIN_PRICE = 50_000;
-  const MAX_PRICE = 10_000_000;
-  const [rangePriceValue, setRangePriceValue] = useState<number>(100_000);
-  //   const [editabelMinPrice, setEditabelMinPrice] = useState<number>(MIN_PRICE);
-  function formatRupiah(value: number): string {
-    return new Intl.NumberFormat("id-ID", {
-      style: "currency",
-      currency: "IDR",
-      minimumFractionDigits: 0,
-    }).format(value);
-  }
+  // const MIN_PRICE = 50_000;
+  // const MAX_PRICE = 10_000_000;
+  // const [rangePriceValue, setRangePriceValue] = useState<number>(100_000);
+  // //   const [editabelMinPrice, setEditabelMinPrice] = useState<number>(MIN_PRICE);
+  // function formatRupiah(value: number): string {
+  //   return new Intl.NumberFormat("id-ID", {
+  //     style: "currency",
+  //     currency: "IDR",
+  //     minimumFractionDigits: 0,
+  //   }).format(value);
+  // }
 
   return (
     <RootLayout>
@@ -29,7 +29,8 @@ export default function DaftarBus() {
             <div className="flex flex-col gap-y-3">
               <div>
                 <h1 className="mb-5">Harga</h1>
-                <Slider
+                <Slider />
+                {/* <Slider
                   min={MIN_PRICE}
                   max={MAX_PRICE}
                   step={50_000}
@@ -39,7 +40,7 @@ export default function DaftarBus() {
                   }
                   className="max-w-xs bg-amber-600"
                 />
-                <h2 className="mt-3">{formatRupiah(rangePriceValue)}</h2>
+                <h2 className="mt-3">{formatRupiah(rangePriceValue)}</h2> */}
               </div>
               <div className="bg-slate-200 h-0.5 rounded-md w-full" />
               <div>
